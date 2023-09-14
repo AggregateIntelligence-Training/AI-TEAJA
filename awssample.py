04197515d1a1212135110ba4d46b61d623302958
AI-TEAJA <teajashree@aggregateintelligence.in>
2023-09-14 12:28:44 +0530

    aws sample program

---
+++ awssample.py
@@ -0,0 +1,8 @@
+import boto3
+client = boto3.client('b3')
+response = client.create_bucket
+{
+    ACL ='private',
+    Bucket = 'javahomecloud123',
+    CreateBucketConfiguration={'LocationConstraint':'ap-south-1'}
+}
--- who.py
+++ who.py
@@ -1,3 +1,2 @@
-a=10
-b=20
-print(a+b)
+mylist=[1,2,3,4,5]
+mylist.pop()
